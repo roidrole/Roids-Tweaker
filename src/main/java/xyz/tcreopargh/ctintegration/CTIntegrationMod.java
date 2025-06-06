b2686590a6fee725d45a1075bb8462e6213d6269
roidrole <roidorle@gmail.com>
2025-06-06 17:43:22 -0400

    Use Tags, remove empty event listeners and forge comments

--- src/main/java/xyz/tcreopargh/ctintegration/CTIntegrationMod.java
+++ src/main/java/xyz/tcreopargh/ctintegration/CTIntegrationMod.java
@@ -4,8 +4,6 @@ import crafttweaker.CraftTweakerAPI;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.tcreopargh.ctintegration.cot.BaubleEventHandler;
 import xyz.tcreopargh.ctintegration.cot.BaubleItemRepresentation;
@@ -13,29 +11,18 @@ import xyz.tcreopargh.ctintegration.cot.BaubleVanillaFactoryExpansion;
 import xyz.tcreopargh.ctintegration.gamestages.events.EventsExpansion;
 
 @Mod(
-        modid = CTIntegrationMod.MOD_ID,
-        name = CTIntegrationMod.MOD_NAME,
-        version = CTIntegrationMod.VERSION,
+        modid = Tags.MOD_ID,
+        name = Tags.MOD_NAME,
+        version = Tags.VERSION,
         dependencies = CTIntegrationMod.DEPENDENCIES
 )
 public class CTIntegrationMod {
 
     public static final String CT_NAMESPACE = "mods.ctintegration.";
-    public static final String MOD_ID = "ctintegration";
-    public static final String MOD_NAME = "CraftTweaker Integration";
-    public static final String VERSION = "1.8.0";
     public static final String DEPENDENCIES = "required-after:crafttweaker";
 
-    /**
-     * This is the instance of your mod as created by Forge. It will never be null.
-     */
-    @Mod.Instance(MOD_ID)
+    @Mod.Instance(Tags.MOD_ID)
     public static CTIntegrationMod INSTANCE;
-
-    /**
-     * This is the first initialization event. Register tile entities here.
-     * The registry events below will have fired prior to entry to this method.
-     */
 
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
@@ -53,19 +40,5 @@ public class CTIntegrationMod {
             CraftTweakerAPI.registerClass(BaubleEventHandler.WillAutoSync.class);
             CraftTweakerAPI.registerClass(BaubleVanillaFactoryExpansion.class);
         }
-    }
-
-    /**
-     * This is the second initialization event. Register custom recipes
-     */
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-    }
-
-    /**
-     * This is the final initialization event. Register actions from other mods here
-     */
-    @Mod.EventHandler
-    public void postinit(FMLPostInitializationEvent event) {
     }
 }
