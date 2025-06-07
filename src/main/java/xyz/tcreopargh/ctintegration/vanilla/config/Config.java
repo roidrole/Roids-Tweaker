0dd3b8d1f2147bad9fb7fdcb00c0497816dc7cdb
roidrole <roidorle@gmail.com>
2025-06-07 08:04:00 -0400

    Option to create new config values

--- src/main/java/xyz/tcreopargh/ctintegration/vanilla/config/Config.java
+++ src/main/java/xyz/tcreopargh/ctintegration/vanilla/config/Config.java
@@ -60,4 +60,94 @@ public class Config {
     public int[] getIntArray(String category, String name) {
         return this.config.getCategory(category).get(name).getIntList();
     }
+
+    @ZenMethod
+    public boolean getBool(String category, String name, boolean defaultvalue) {
+        return this.config.getCategory(category).get(name).getBoolean(defaultvalue);
+    }
+
+    @ZenMethod
+    public boolean getBoolean(String category, String name, boolean defaultvalue) {
+        return this.config.getCategory(category).get(name).getBoolean(defaultvalue);
+    }
+
+    @ZenMethod
+    public String getString(String category, String name, String defaultvalue) {
+        return this.config.get(category, name, defaultvalue).getString();
+    }
+
+    @ZenMethod
+    public int getInt(String category, String name, int defaultvalue) {
+        return this.config.getCategory(category).get(name).getInt(defaultvalue);
+    }
+
+    @ZenMethod
+    public double getDouble(String category, String name, double defaultvalue) {
+        return this.config.getCategory(category).get(name).getDouble(defaultvalue);
+    }
+
+    @ZenMethod
+    public long getLong(String category, String name, long defaultvalue) {
+        return this.config.getCategory(category).get(name).getLong(defaultvalue);
+    }
+
+    @ZenMethod
+    public boolean[] getBooleanArray(String category, String name, boolean[] defaultvalue) {
+        return this.config.get(category, name, defaultvalue).getBooleanList();
+    }
+
+    @ZenMethod
+    public String[] getStringArray(String category, String name, String[] defaultvalue) {
+        return this.config.get(category, name, defaultvalue).getStringList();
+    }
+
+    @ZenMethod
+    public int[] getIntArray(String category, String name, int[] defaultvalue) {
+        return this.config.get(category, name, defaultvalue).getIntList();
+    }
+
+    @ZenMethod
+    public boolean getBool(String category, String name, boolean defaultvalue, String comment) {
+        return this.config.get(category, name, comment).getBoolean(defaultvalue);
+    }
+
+    @ZenMethod
+    public boolean getBoolean(String category, String name, boolean defaultvalue, String comment) {
+        return this.config.get(category, name, comment).getBoolean(defaultvalue);
+    }
+
+    @ZenMethod
+    public String getString(String category, String name, String defaultvalue, String comment) {
+        return this.config.get(category, name, defaultvalue, comment).getString();
+    }
+
+    @ZenMethod
+    public int getInt(String category, String name, int defaultvalue, String comment) {
+        return this.config.get(category, name, comment).getInt(defaultvalue);
+    }
+
+    @ZenMethod
+    public double getDouble(String category, String name, double defaultvalue, String comment) {
+        return this.config.get(category, name, comment).getDouble(defaultvalue);
+    }
+
+    @ZenMethod
+    public long getLong(String category, String name, long defaultvalue, String comment) {
+        return this.config.get(category, name, comment).getLong(defaultvalue);
+    }
+
+    @ZenMethod
+    public boolean[] getBooleanArray(String category, String name, boolean[] defaultvalue, String comment) {
+        return this.config.get(category, name, defaultvalue, comment).getBooleanList();
+    }
+
+    @ZenMethod
+    public String[] getStringArray(String category, String name, String[] defaultvalue, String comment) {
+        return this.config.get(category, name, defaultvalue, comment).getStringList();
+    }
+
+    @ZenMethod
+    public int[] getIntArray(String category, String name, int[] defaultvalue, String comment) {
+        return this.config.get(category, name, defaultvalue, comment).getIntList();
+    }
 }
