a0b4dd3129b942d225a0a91481127873855170d1
roidrole <roidorle@gmail.com>
2025-06-10 10:30:32 -0400

    Use CT's @Optional for Config, add min/max for numerical values, add length for array

--- src/main/java/roidrole/roidtweaker/mods/forge/config/Config.java
+++ src/main/java/roidrole/roidtweaker/mods/forge/config/Config.java
@@ -3,6 +3,7 @@ package roidrole.roidtweaker.mods.forge.config;
 import crafttweaker.annotations.ZenRegister;
 import net.minecraftforge.common.config.Configuration;
 import roidrole.roidtweaker.RoidTweaker;
+import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -16,137 +17,86 @@ public class Config {
     }
 
     @ZenMethod
-    public boolean getBool(String category, String name) {
-        return this.config.getCategory(category).get(name).getBoolean();
+    public boolean getBool(String category, String name, @Optional Boolean defaultValue, @Optional String comment) {
+        if(defaultValue == null){
+            return this.config.getCategory(category).get(name).getBoolean();
+        }
+        return this.config.get(category, name, defaultValue, comment).getBoolean();
     }
 
     @ZenMethod
-    public boolean getBoolean(String category, String name) {
-        return this.config.getCategory(category).get(name).getBoolean();
+    public boolean getBoolean(String category, String name, @Optional Boolean defaultValue, @Optional String comment) {
+        if(defaultValue == null){
+            return this.config.getCategory(category).get(name).getBoolean();
+        }
+        return this.config.get(category, name, defaultValue, comment).getBoolean();
     }
 
     @ZenMethod
-    public String getString(String category, String name) {
-        return this.config.getCategory(category).get(name).getString();
+    public String getString(String category, String name, @Optional String defaultValue, @Optional String comment) {
+        if(defaultValue == null){
+            return this.config.getCategory(category).get(name).getString();
+        }
+        return this.config.get(category, name, defaultValue, comment).getString();
     }
 
     @ZenMethod
-    public int getInt(String category, String name) {
-        return this.config.getCategory(category).get(name).getInt();
+    public int getInt(String category, String name, @Optional Integer defaultValue, @Optional String comment, @Optional Integer min, @Optional Integer max) {
+        if(defaultValue == null){
+            return this.config.getCategory(category).get(name).getInt();
+        }
+        if(min != null && max != null){
+            this.config.get(category, name, defaultValue, comment, min, max).getInt();
+        }
+        return this.config.get(category, name, defaultValue, comment).getInt();
     }
 
     @ZenMethod
-    public double getDouble(String category, String name) {
-        return this.config.getCategory(category).get(name).getDouble();
+    public double getDouble(String category, String name, @Optional Double defaultValue, @Optional String comment, @Optional Double min, @Optional Double max) {
+        if(defaultValue == null){
+            return this.config.getCategory(category).get(name).getDouble();
+        }
+        if(min != null && max != null){
+            this.config.get(category, name, defaultValue, comment, min, max).getDouble();
+        }
+        return this.config.get(category, name, defaultValue, comment).getDouble();
     }
 
     @ZenMethod
-    public long getLong(String category, String name) {
-        return this.config.getCategory(category).get(name).getLong();
+    public long getLong(String category, String name, @Optional Long defaultValue, @Optional String comment, @Optional Long min, @Optional Long max) {
+        if(defaultValue == null){
+            return this.config.getCategory(category).get(name).getLong();
+        }
+        if(min != null && max != null){
+            this.config.get(category, name, defaultValue, comment, min, max).getLong();
+        }
+        return this.config.get(category, name, defaultValue, comment).getLong();
     }
 
     @ZenMethod
-    public boolean[] getBooleanArray(String category, String name) {
-        return this.config.getCategory(category).get(name).getBooleanList();
+    public boolean[] getBooleanArray(String category, String name, @Optional boolean[] defaultValue, @Optional String comment, @Optional boolean isListLengthFixed, @Optional("1") int maxListLength) {
+        if(defaultValue.length == 0){
+            return this.config.getCategory(category).get(name).getBooleanList();
+        }
+        return this.config.get(category, name, defaultValue, comment, isListLengthFixed, maxListLength).getBooleanList();
     }
 
     @ZenMethod
-    public String[] getStringArray(String category, String name) {
-        return this.config.getCategory(category).get(name).getStringList();
+    public String[] getStringArray(String category, String name, @Optional String[] defaultValue, @Optional String comment, @Optional boolean isListLengthFixed, @Optional("1") int maxListLength) {
+        if(defaultValue.length == 0){
+            return this.config.getCategory(category).get(name).getStringList();
+        }
+        return this.config.get(category, name, defaultValue, comment, isListLengthFixed, maxListLength, null).getStringList();
     }
 
     @ZenMethod
-    public int[] getIntArray(String category, String name) {
-        return this.config.getCategory(category).get(name).getIntList();
-    }
-
-    @ZenMethod
-    public boolean getBool(String category, String name, boolean defaultvalue) {
-        return this.config.getCategory(category).get(name).getBoolean(defaultvalue);
-    }
-
-    @ZenMethod
-    public boolean getBoolean(String category, String name, boolean defaultvalue) {
-        return this.config.getCategory(category).get(name).getBoolean(defaultvalue);
-    }
-
-    @ZenMethod
-    public String getString(String category, String name, String defaultvalue) {
-        return this.config.get(category, name, defaultvalue).getString();
-    }
-
-    @ZenMethod
-    public int getInt(String category, String name, int defaultvalue) {
-        return this.config.getCategory(category).get(name).getInt(defaultvalue);
-    }
-
-    @ZenMethod
-    public double getDouble(String category, String name, double defaultvalue) {
-        return this.config.getCategory(category).get(name).getDouble(defaultvalue);
-    }
-
-    @ZenMethod
-    public long getLong(String category, String name, long defaultvalue) {
-        return this.config.getCategory(category).get(name).getLong(defaultvalue);
-    }
-
-    @ZenMethod
-    public boolean[] getBooleanArray(String category, String name, boolean[] defaultvalue) {
-        return this.config.get(category, name, defaultvalue).getBooleanList();
-    }
-
-    @ZenMethod
-    public String[] getStringArray(String category, String name, String[] defaultvalue) {
-        return this.config.get(category, name, defaultvalue).getStringList();
-    }
-
-    @ZenMethod
-    public int[] getIntArray(String category, String name, int[] defaultvalue) {
-        return this.config.get(category, name, defaultvalue).getIntList();
-    }
-
-    @ZenMethod
-    public boolean getBool(String category, String name, boolean defaultvalue, String comment) {
-        return this.config.get(category, name, comment).getBoolean(defaultvalue);
-    }
-
-    @ZenMethod
-    public boolean getBoolean(String category, String name, boolean defaultvalue, String comment) {
-        return this.config.get(category, name, comment).getBoolean(defaultvalue);
-    }
-
-    @ZenMethod
-    public String getString(String category, String name, String defaultvalue, String comment) {
-        return this.config.get(category, name, defaultvalue, comment).getString();
-    }
-
-    @ZenMethod
-    public int getInt(String category, String name, int defaultvalue, String comment) {
-        return this.config.get(category, name, comment).getInt(defaultvalue);
-    }
-
-    @ZenMethod
-    public double getDouble(String category, String name, double defaultvalue, String comment) {
-        return this.config.get(category, name, comment).getDouble(defaultvalue);
-    }
-
-    @ZenMethod
-    public long getLong(String category, String name, long defaultvalue, String comment) {
-        return this.config.get(category, name, comment).getLong(defaultvalue);
-    }
-
-    @ZenMethod
-    public boolean[] getBooleanArray(String category, String name, boolean[] defaultvalue, String comment) {
-        return this.config.get(category, name, defaultvalue, comment).getBooleanList();
-    }
-
-    @ZenMethod
-    public String[] getStringArray(String category, String name, String[] defaultvalue, String comment) {
-        return this.config.get(category, name, defaultvalue, comment).getStringList();
-    }
-
-    @ZenMethod
-    public int[] getIntArray(String category, String name, int[] defaultvalue, String comment) {
-        return this.config.get(category, name, defaultvalue, comment).getIntList();
+    public int[] getIntArray(String category, String name, @Optional int[] defaultValue, @Optional String comment, @Optional Integer min, @Optional Integer max, @Optional boolean isListLengthFixed, @Optional("1") int maxListLength) {
+        if(defaultValue.length == 0){
+            return this.config.getCategory(category).get(name).getIntList();
+        }
+        if(min != null && max != null){
+            this.config.get(category, name, defaultValue, comment, min, max, isListLengthFixed, maxListLength).getIntList();
+        }
+        return this.config.get(category, name, defaultValue, comment).getIntList();
     }
 }
